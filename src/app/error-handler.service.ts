c116c27f6c94d0beabfb69d7f07e84b31832b117
jproteau <julie.proteau@hotmail.com>
2019-01-09 12:39:07 -0500

    Create error-handler.service.ts
    Begin error handling 3

---
+++ src/app/error-handler.service.ts
@@ -0,0 +1,31 @@
+import { ErrorHandler, Injectable } from '@angular/core';
+import { MessageService } from './message.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorHandlerService implements ErrorHandler {
+
+  constructor(private messageService: MessageService, private translate: TranslateService) { }
+
+  handleError(error: Error | HttpErrorResponse) {
+     if (error instanceof HttpErrorResponse) {
+
+        if (error.status == 0) {
+          this.messageService.showError(this.translate.instant('error.api.down'));
+        } else {
+          if (error.status == 404) {
+            this.messageService.showError(this.translate.instant('error.api.notfound'));
+          } else {
+            this.messageService.showError(this.translate.instant('error.api.general'));
+          }
+        }
+     } else {
+        this.messageService.showError(this.translate.instant('error.general'));
+     }
+
+    console.error('It happens: ', error);
+  }
+}
